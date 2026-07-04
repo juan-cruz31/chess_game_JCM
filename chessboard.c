2c096d68a70df870ee39f1fc67630e0ba2d84bfc
Juan Cruz Moras <jcruzmoras@mi.unc.edu.ar>
2026-07-04 15:19:00 -0300

    tablero

--- chessboard.c
+++ chessboard.c
@@ -1,8 +1,25 @@
+#include <stdio.h>
 
 
+// These libraries allow the pieces to be loaded, as they are larger than an ASCII character
+#include <wchar.h>
+#include <locale.h>
 
 
+// Unicode Chess Symbols
+// White: ♚ ♛ ♜ ♝ ♞ ♟
+// Black: ♔ ♕ ♖ ♗ ♘ ♙
 
+wchar_t tablero[8][8]=
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
+    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '}
+};
 
 
 
