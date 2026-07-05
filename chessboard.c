663bb1bab8b095254219c9612285a5d8412b3bc7
Juan Cruz Moras <jcruzmoras@mi.unc.edu.ar>
2026-07-04 21:34:12 -0300

    Print incompleto, User.c

--- chessboard.c
+++ chessboard.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
 
 // These libraries allow the pieces to be loaded, as they are larger than an ASCII character
 #include <wchar.h>
 #include <locale.h>
 
+#define SIZE_BOARD 8
 
 // Unicode Chess Symbols
 // White: ♚ ♛ ♜ ♝ ♞ ♟
 // Black: ♔ ♕ ♖ ♗ ♘ ♙
 
-wchar_t tablero[8][8]=
+wchar_t board[SIZE_BOARD][SIZE_BOARD]={
     {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
     {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
     {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
@@ -22,7 +25,13 @@ wchar_t tablero[8][8]=
 };
 
 
-
+void print_board(wchar_t board[SIZE_BOARD][SIZE_BOARD]){
+    for(int row = 0; row < SIZE_BOARD; row++){
+        for(int column = 0; column < SIZE_BOARD; column++){
+            wprintf(L"%c", board[row][column]);
+        }
+    }
+}
 
 
 
---
+++ user.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+
+enum Color {White, Black};
+
+typedef struct _user
+{
+
+    bool _check;
+    bool _checkMate;
+} User;
+
+
+
+
+
+
+
+
+
+
