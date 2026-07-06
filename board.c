7c4938b72b7c36f6a464154dcd01c6fca476fc05
Juan Cruz Moras <jcruzmoras@mi.unc.edu.ar>
2026-07-06 09:15:56 -0300

    New board

--- board.c
+++ board.c
@@ -1,37 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <assert.h>
-
-// These libraries allow the pieces to be loaded, as they are larger than an ASCII character
-#include <wchar.h>
-#include <locale.h>
-
-#define SIZE_BOARD 8
-
-// Unicode Chess Symbols
-// White: ♚ ♛ ♜ ♝ ♞ ♟
-// Black: ♔ ♕ ♖ ♗ ♘ ♙
-
-wchar_t board[SIZE_BOARD][SIZE_BOARD]={
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '},
-    {L' ', L' ', L' ', L' ', L' ', L' ', L' ', L' '}
-};
+#include "board.h"
 
 
-void print_board(wchar_t board[SIZE_BOARD][SIZE_BOARD]){
-    for(int row = 0; row < SIZE_BOARD; row++){
-        for(int column = 0; column < SIZE_BOARD; column++){
-            wprintf(L"%c", board[row][column]);
-        }
-    }
-}
 
 
 
--- board.h
+++ board.h
@@ -1,12 +1,41 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <stdbool.h>
+
+#define BOARD_SIZE 8
+
+
+
+typedef enum = {
+    EMPTY = 0,
+    W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING,
+    B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING
+} Piece;
+
+
+
+typedef struct {
+    Piece cells[BOARD_SIZE][BOARD_SIZE];
+    int w_b_turn;
+
+    bool white_can_castle_kingside;
+    bool white_can_castle_queenside;
+
+    bool black_can_castle_kingside;
+    bool black_can_castle_queenside;
+
+    //other rules
+} ChessBoard;
 
 
 
 
+void clear_board(ChessBoard *board);
 
+void load_start_position(ChessBoard *board);
 
-
-
-
+#endif
 
 
 
